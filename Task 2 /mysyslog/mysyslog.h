0feb7ee34851ca734194a2840cf037ab70af2b41
Ivan <150362093+MercenaryGeneral@users.noreply.github.com>
2024-11-05 11:28:05 +0300

    Create mysyslog.h

---
+++ Task 2 /mysyslog/mysyslog.h
@@ -0,0 +1,30 @@
+// /mysyslog.h
+
+#ifndef MYSYSLOG_H
+#define MYSYSLOG_H
+
+// Уровни журналирования
+typedef enum {
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR,
+    CRITICAL
+} LogLevel;
+
+// Форматы вывода
+typedef enum {
+    TEXT,
+    JSON
+} LogFormat;
+
+// Драйверы вывода
+typedef enum {
+    DRIVER_CONSOLE,
+    DRIVER_FILE
+} LogDriver;
+
+// Функция для записи в журнал
+int mysyslog(const char* msg, LogLevel level, LogDriver driver, LogFormat format, const char* path);
+
+#endif // MYSYSLOG_H
