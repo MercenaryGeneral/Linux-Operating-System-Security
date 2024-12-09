27439c8661ab1a26d3b145cd12b294dae8bbd82d
Ivan <150362093+MercenaryGeneral@users.noreply.github.com>
2024-12-09 19:22:59 +0300

    Update driver.c

--- Task 2 /mysyslog/libmysyslog/mysyslog_text/driver.c
+++ Task 2 /mysyslog/libmysyslog/mysyslog_text/driver.c
@@ -1,34 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-#include "Task 2 /mysyslog/libmysyslog/mysyslog.h"
+#include "text_driver.h"
 
-void
-init_text_driver(void) {
-}
+void log_to_text(const char* msg, int level, const char* process) {
+    time_t now = time(NULL);
+    char* timestamp = ctime(&now);
+    timestamp[strlen(timestamp) - 1] = '\0'; // Удаляем символ новой строки
 
-void
-log_text(const char* msg, mysyslog_level_t level, const char* path) {
-	time_t now = time(NULL);
-	struct tm* tm = localtime(&now);
-
-	FILE* f = fopen(path, "a");
-	if (f) {
-		fprintf(f, "%li %s process %s %s\n", (long)now, (level == MYSYSLOG_DEBUG ? "DEBUG" :
-											level == MYSYSLOG_INFO ? "INFO" :
-											level == MYSYSLOG_WARN ? "WARN" :
-											level == MYSYSLOG_ERROR ? "ERROR" :
-											"CRITICAL"),
-jeta("message", msg));
-		fclose(f);
-	}
-}
-
-mysyslog_driver_t driver_text = {
-	.init = init_text_driver,
-	.log = log_text
-};
-
-void
-init(void) {
-	register_driver(&driver_text);
+    printf("%s %s %s\n", timestamp, log_level_to_string(level), msg);
 }
